3dadc7ca41bbf98cecf7b9fd2daa2d7b8e7bde5b
David Johan Hasiholan Parhusip <david.johanhp@gmail.com>
2024-03-04 13:09:29 +0700

    feat/footer: add footer, constants

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { footerLinks } from '@/constants'
+
 const Footer = () => {
   return (
     <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
@@ -14,7 +16,40 @@ const Footer = () => {
                     height={18}
                     className='object-contain'
                 />
+                <p className='text-base text-gray-700'>
+                  Car Hub 2023 <br />
+                  All rights reversed &copy;
+                </p>
             </div>
+
+            <div className='footer__links'>
+              {footerLinks.map((link) => (
+                <div key={link.title} className='footer__link'>
+                  <h3 className='font-bold'>{link.title}</h3>
+                  {link.links.map((item) => (
+                    <Link
+                      key={item.title}
+                      href={item.url}
+                      className="text-gray-500" 
+                    >
+                      {item.title}
+                      </Link>
+                  ))}
+                </div>
+              ))}
+            </div>
+        </div>
+        
+        <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+          <p>@2023 CarHub. All Rights Reserved</p>
+          <div className='footer__copyright-link'>
+            <Link href="/" className='text-gray-500'>
+              Privacy Policy
+            </Link>
+            <Link href="/" className='text-gray-500'>
+              Terms of Use
+            </Link>
+          </div>
         </div>
     </footer>
   )
